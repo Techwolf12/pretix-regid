93e9267ab643343783c20bfbdc4ccc9fdef55fa0
techwolf12 <contact@techwolf12.nl>
2023-11-12 13:49:24 +0100

    Update to modern standards

---
+++ pretix_regid/apps.py
@@ -0,0 +1,24 @@
+from django.utils.translation import gettext_lazy
+from . import __version__
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+class PluginApp(PluginConfig):
+    name = "pretix_regid"
+    verbose_name = "Registration ID"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Registration ID")
+        author = "Christiaan de Die le Clercq (techwolf12)"
+        description = gettext_lazy(
+            "Pretix plugin to add an automatic registration ID to approved orders"
+        )
+        visible = True
+        version = __version__
+        category = "FEATURE"
+        compatibility = "pretix>=2.7"
+
+    def ready(self):
+        from . import signals  # NOQA
